f0dad9a36346cd3acf258778a3366ae616fce25f
ardorleo <ardorleo@b38e4279-40fa-c1fe-6c80-91d194a5d2a9>
2013-06-16 16:02:44 +0000

    增加ApplicationContext.xml配置，包含了Druid、MyBatis、Transaction。等待测试。 准备以RBAC理论，做个完整的测试例子。包括权限结构、权限存储、权限授予、权限使用

--- zh-l/StdWeb/src/main/java/com/std/web/MsgController.java
+++ zh-l/StdWeb/src/main/java/com/std/web/MsgController.java
@@ -9,10 +9,20 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MsgController {
 
-    @RequestMapping("/msg.htm")
+    @RequestMapping("/msgx.htm")
     public ModelAndView redirect(HttpServletRequest request) {
     	ModelAndView pageModel=new ModelAndView("msgx");
     	pageModel.addObject("msg", "Hello spring mvc, tiles, velocity!");
+    	pageModel.addObject("flag","7");
+    	request.setAttribute("test","test");
         return pageModel;
+    }
+    @RequestMapping("/msg.htm")
+    public String redirectNew(HttpServletRequest request) {
+    	ModelAndView pageModel=new ModelAndView("msgx");
+    	pageModel.addObject("msg", "Hello spring mvc, tiles, velocity!");
+    	pageModel.addObject("flag","7");
+    	request.setAttribute("test","test");
+        return "msgx";
     }
 }
