436a428fbe09cabed7ea242334efc87d6ee30bb7
ardorleo <ardorleo@b38e4279-40fa-c1fe-6c80-91d194a5d2a9>
2013-06-16 16:13:10 +0000

    增加ApplicationContext.xml配置，包含了Druid、MyBatis、Transaction。等待测试。 准备以RBAC理论，做个完整的测试例子。包括权限结构、权限存储、权限授予、权限使用

---
+++ trunk/zh-l/StdWeb/src/main/java/com/std/entity/User.java
@@ -0,0 +1,121 @@
+package com.std.entity;
+
+import java.beans.Transient;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.collect.Lists;
+import com.std.utils.Collections3;
+
+/**
+ * 用户.
+ *
+ * @author calvin
+ */
+
+public class User extends IdEntity {
+	private String loginName;
+	private String plainPassword;
+	private String password;
+	private String salt;
+	private String name;
+	private String email;
+	private String status;
+
+	private Team team;
+
+	private List<Role> roleList = Lists.newArrayList(); // 有序的关联对象集合
+
+	@NotBlank
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	@Transient
+	public String getPlainPassword() {
+		return plainPassword;
+	}
+
+	public void setPlainPassword(String plainPassword) {
+		this.plainPassword = plainPassword;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	@NotBlank
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Email
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public List<Role> getRoleList() {
+		return roleList;
+	}
+
+	public void setRoleList(List<Role> roleList) {
+		this.roleList = roleList;
+	}
+
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+
+	@Transient
+	@JsonIgnore
+	public String getRoleNames() {
+		return Collections3.extractToString(roleList, "name", ", ");
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+}
