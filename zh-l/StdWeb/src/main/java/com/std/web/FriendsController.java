d9b5ad9d652ca7ee07b5c33dd13f6fc543288535
ardorleo <ardorleo@b38e4279-40fa-c1fe-6c80-91d194a5d2a9>
2013-06-23 14:02:43 +0000

    增加切面Aspect缺少的Jar依赖。 同时，似乎解决了，service实例在controller里只有注解没有set方法时，运行不成功的问题

--- zh-l/StdWeb/src/main/java/com/std/web/FriendsController.java
+++ zh-l/StdWeb/src/main/java/com/std/web/FriendsController.java
@@ -1,9 +1,14 @@
 package com.std.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.std.entity.User;
 import com.std.service.AccountEffectiveService;
 
 @Controller
@@ -11,13 +16,11 @@ public class FriendsController {
 	@Autowired
 	private AccountEffectiveService accountService;
 
-	public void setAccountService(AccountEffectiveService accountService) {
-		this.accountService = accountService;
-	}
-
 	@RequestMapping("/friends.htm")
-	public String redirect() {
-		accountService.deleteUser(123456L);
+	public String redirect() throws JsonProcessingException {
+		List<User> userList=accountService.searchUser("spring", "spring_hello");
+		ObjectMapper om=new ObjectMapper();
+		System.out.println(om.writeValueAsString(userList));
 		return "friends";
 	}
 }
